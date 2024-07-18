f38523c0ffaabcc331513b7ae0a2c08d9d9039c1
Tobias Fella <tobias.fella@gnupg.com>
2024-07-18 11:05:24 +0200

    Show better error messages for invalid certificates in certificatelineedit
    GnuPG-Bug-ID: 7183
    (cherry picked from commit 91856657a925316b8bf3a2c909ef1f067bd283f1)

--- src/crypto/gui/certificatelineedit.cpp
+++ src/crypto/gui/certificatelineedit.cpp
@@ -205,6 +205,8 @@ public:
         Success, //< a certificate or group is set
         None, //< entered text does not match any certificates or groups
         Ambiguous, //< entered text matches multiple certificates or groups
+        Revoked, //< the selected cert is revoked
+        Expired, //< the selected cert is expired
     };
     enum class CursorPositioning {
         MoveToEnd,
@@ -589,7 +591,13 @@ void CertificateLineEdit::Private::updateKey(CursorPositioning positioning)
                 mStatus = Status::None;
             }
         } else {
-            mStatus = Status::None;
+            if (!mUserId.isNull() && (mUserId.isRevoked() || mUserId.parent().isRevoked())) {
+                mStatus = Status::Revoked;
+            } else if (!mUserId.isNull() && mUserId.parent().isExpired()) {
+                mStatus = Status::Expired;
+            } else {
+                mStatus = Status::None;
+            }
         }
     }
     mKey = newKey;
@@ -636,6 +644,10 @@ QString CertificateLineEdit::Private::errorMessage() const
         return i18n("No matching certificates or groups found");
     case Status::Ambiguous:
         return i18n("Multiple matching certificates or groups found");
+    case Status::Expired:
+        return i18n("This certificate is expired");
+    case Status::Revoked:
+        return i18n("This certificate is revoked");
     default:
         qDebug(KLEOPATRA_LOG) << __func__ << "Invalid status:" << static_cast<int>(mStatus);
         Q_ASSERT(!"Invalid status");
@@ -666,6 +678,9 @@ QIcon CertificateLineEdit::Private::statusIcon() const
         } else {
             return QIcon::fromTheme(QStringLiteral("emblem-error"));
         }
+    case Status::Expired:
+    case Status::Revoked:
+        return QIcon::fromTheme(QStringLiteral("emblem-error"));
     default:
         qDebug(KLEOPATRA_LOG) << __func__ << "Invalid status:" << static_cast<int>(mStatus);
         Q_ASSERT(!"Invalid status");
