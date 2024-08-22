611c53c5fa4adbbbabe5e3d178057ddf1f08a06e
Ingo Klöcker <dev@ingo-kloecker.de>
2024-08-22 17:22:20 +0200

    Move update of NullPIN message to common smart card widget
    GnuPG-bug-id: 7018
    (cherry picked from commit c32edd9ab3a86eaf1ef63f7ab893910d7fbaea6c)

--- src/view/netkeywidget.cpp
+++ src/view/netkeywidget.cpp
@@ -7,23 +7,10 @@
 */
 #include "netkeywidget.h"
 
-#include "cardkeysview.h"
-#include "smartcardactions.h"
-
-#include "kleopatra_debug.h"
-
-#include "smartcard/netkeycard.h"
-
-#include <utils/qt6compat.h>
-
-#include <KLocalizedString>
-#include <KMessageWidget>
-
-#include <QIcon>
+#include <smartcard/card.h>
 
 using namespace Kleo;
 using namespace Kleo::SmartCard;
-using namespace Qt::Literals::StringLiterals;
 
 NetKeyWidget::NetKeyWidget(QWidget *parent)
     : SmartCardWidget(AppType::NetKeyApp, parent)
@@ -31,30 +18,3 @@ NetKeyWidget::NetKeyWidget(QWidget *parent)
 }
 
 NetKeyWidget::~NetKeyWidget() = default;
-
-void NetKeyWidget::setCard(const NetKeyCard *card)
-{
-    SmartCardWidget::setCard(card);
-
-    /* Only check for the standard NKS NullPIN.
-     * According to users of NetKey cards it is fairly uncommon to use SigG certificates at all.
-     * So it should be optional to set the SigG pins. */
-    auto nullPinWidget = nullPinMessageWidget();
-    if (card->hasNKSNullPin()) {
-        nullPinWidget->setMessageType(KMessageWidget::Information);
-        nullPinWidget->setIcon(QIcon::fromTheme(u"data-information"_s));
-        const auto nullTitle = i18nc(
-            "NullPIN is a word that is used all over in the netkey "
-            "documentation and should be understandable by Netkey cardholders",
-            "The NullPIN is still active on this card.");
-        const auto nullDescription = i18n("You need to set a PIN before you can use the certificates.");
-        nullPinWidget->setText(QStringLiteral("<b>%1</b><br/>%2").arg(nullTitle, nullDescription));
-        nullPinWidget->setCloseButtonVisible(false);
-        if (nullPinWidget->actions().isEmpty()) {
-            nullPinWidget->addAction(SmartCardActions::createProxyAction(SmartCardActions::instance()->action(u"card_netkey_set_nks_pin"_s), nullPinWidget));
-        }
-        nullPinWidget->setVisible(true);
-    } else {
-        nullPinWidget->setVisible(false);
-    }
-}
--- src/view/smartcardwidget.cpp
+++ src/view/smartcardwidget.cpp
@@ -159,6 +159,34 @@ static void updateCardActions(QToolButton *actionsButton, const Card *card)
     }
 }
 
+static void updateNullPinWidget(KMessageWidget *nullPinWidget, const Card *card)
+{
+    Q_ASSERT(card);
+    if (card->appType() != AppType::NetKeyApp) {
+        return;
+    }
+    /* Only check for the standard NKS NullPIN.
+     * According to users of NetKey cards it is fairly uncommon to use SigG certificates at all.
+     * So it should be optional to set the SigG pins. */
+    if (card->hasNKSNullPin()) {
+        nullPinWidget->setMessageType(KMessageWidget::Information);
+        nullPinWidget->setIcon(QIcon::fromTheme(u"data-information"_s));
+        const auto nullTitle = i18nc(
+            "NullPIN is a word that is used all over in the netkey "
+            "documentation and should be understandable by Netkey cardholders",
+            "The NullPIN is still active on this card.");
+        const auto nullDescription = i18n("You need to set a PIN before you can use the certificates.");
+        nullPinWidget->setText(QStringLiteral("<b>%1</b><br/>%2").arg(nullTitle, nullDescription));
+        nullPinWidget->setCloseButtonVisible(false);
+        if (nullPinWidget->actions().isEmpty()) {
+            nullPinWidget->addAction(SmartCardActions::createProxyAction(SmartCardActions::instance()->action(u"card_netkey_set_nks_pin"_s), nullPinWidget));
+        }
+        nullPinWidget->setVisible(true);
+    } else {
+        nullPinWidget->setVisible(false);
+    }
+}
+
 SmartCardWidget::SmartCardWidget(Kleo::SmartCard::AppType appType, QWidget *parent)
     : QWidget{parent}
     , mAppType{appType}
@@ -291,6 +319,8 @@ void SmartCardWidget::setCard(const Card *card)
 
     updateCardActions(mCardActionsButton, card);
 
+    updateNullPinWidget(mNullPinWidget, card);
+
     const auto errMsg = card->errorMsg();
     if (!errMsg.isEmpty()) {
         mErrorWidget->setMessageType(KMessageWidget::Error);
@@ -333,11 +363,6 @@ GpgME::Key SmartCardWidget::currentCertificate() const
         return mCardKeysView->currentCertificate();
     }
     return {};
-}
-
-KMessageWidget *SmartCardWidget::nullPinMessageWidget() const
-{
-    return mNullPinWidget;
 }
 
 QLabel *SmartCardWidget::statusLabel() const
