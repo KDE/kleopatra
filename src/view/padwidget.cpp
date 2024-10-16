9fe816aea5edcc050948dbc50a3dffd74688003e
Carl Schwan <carl@carlschwan.eu>
2024-10-16 10:35:00 +0200

    notepad: Improve accessibility of KMessageWidget
    Announce to screen reader when the message widget appear.

--- src/view/padwidget.cpp
+++ src/view/padwidget.cpp
@@ -35,6 +35,7 @@
 
 #include <QGpgME/DataProvider>
 
+#include <QAccessible>
 #include <QButtonGroup>
 #include <QFontDatabase>
 #include <QFontMetrics>
@@ -522,6 +523,11 @@ public:
                 mCryptBtn->setEnabled(false);
             }
             mMessageWidget->setVisible(!DeVSCompliance::isCompliant());
+#if KWIDGETSADDONS_VERSION >= QT_VERSION_CHECK(6, 8, 0)
+            if (mMessageWidget->isVisible() && QAccessible::isActive()) {
+                mMessageWidget->setFocus();
+            }
+#endif
         }
     }
 
