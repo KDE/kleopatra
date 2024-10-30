7837bf71545d534cfc1f3b89c31a4b97d1cba8be
Ingo Klöcker <dev@ingo-kloecker.de>
2024-10-30 14:28:17 +0100

    Use the copyright symbol instead of (C)
    And use the Unicode code point instead of the UTF-8 code point for KDAB's
    umlaut as in Kleopatra's About dialog.
    
    GnuPG-bug-id: 7329

--- src/kwatchgnupg/aboutdata.cpp
+++ src/kwatchgnupg/aboutdata.cpp
@@ -31,8 +31,8 @@ AboutData::AboutData()
                  QLatin1StringView(KWATCHGNUPG_VERSION_STRING),
                  i18nc("@info", "Viewer for GnuPG daemon and application logs"),
                  KAboutLicense::GPL,
-                 i18nc("@info:credit", "(C) 2019-%1 g10 Code GmbH", QStringLiteral("2024")) + QLatin1Char('\n')
-                     + i18n("(C) 2001-2004 Klar\xC3\xA4lvdalens Datakonsult AB\n"))
+                 i18nc("@info:credit", "\u00A9 2019-%1 g10 Code GmbH", QStringLiteral("2024")) + QLatin1Char('\n')
+                     + i18n("\u00A9 2001-2004 Klar\u00E4lvdalens Datakonsult AB\n"))
 {
     using ::authors;
     for (const auto &author : authors) {
