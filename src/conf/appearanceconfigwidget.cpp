4f2fc4ee9caf71ca7942d17b92a4d47a0670843c
Ingo Klöcker <dev@ingo-kloecker.de>
2025-06-26 11:47:48 +0200

    Do not write filter names to libkleopatrarc
    Users are not allowed to change the names of the filters and they cannot
    create new filters. And writing the filter names to libkleopatrarc had
    the unwanted side-effect that this burned translated filter names into
    the libkleopatrarc overwriting the English default names.
    
    GnuPG-bug-id: 7612
    
    (cherry picked from commit 22add27cc645395edc5eecbef936097997c50898)

--- src/conf/appearanceconfigwidget.cpp
+++ src/conf/appearanceconfigwidget.cpp
@@ -49,10 +49,8 @@ using namespace Kleo;
 using namespace Kleo::Config;
 
 enum {
-    HasNameRole = Qt::UserRole + 0x1234, /*!< Records that the user has assigned a name (to avoid comparing with i18n-strings) */
-    HasFontRole, /*!< Records that the user has chosen  completely different font (as opposed to italic/bold/strikeout) */
+    HasFontRole = Qt::UserRole + 0x1234, /*!< Records that the user has chosen  completely different font (as opposed to italic/bold/strikeout) */
     IconNameRole, /*!< Records the name of the icon (since QIcon won't give it out again, once set) */
-    MayChangeNameRole,
     MayChangeForegroundRole,
     MayChangeBackgroundRole,
     MayChangeFontRole,
@@ -140,8 +138,6 @@ static void apply_config(const KConfigGroup &group, QListWidgetItem *item)
 
     const QString name = group.readEntry("Name");
     item->setText(name.isEmpty() ? i18nc("Key filter without user-assigned name", "<unnamed>") : name);
-    item->setData(HasNameRole, !name.isEmpty());
-    item->setData(MayChangeNameRole, !group.isEntryImmutable("Name"));
 
     const QColor fg = group.readEntry("foreground-color", QColor());
     item->setData(StoredForegroundRole, fg.isValid() ? QBrush(fg) : QVariant());
@@ -278,7 +274,6 @@ static void save_to_config(const QListWidgetItem *item, KConfigGroup &group)
     if (!item) {
         return;
     }
-    writeOrDelete(group, "Name", item->data(HasNameRole).toBool() ? item->text() : QVariant());
     writeOrDelete(group, "foreground-color", brush2color(item->data(StoredForegroundRole)));
     writeOrDelete(group, "background-color", brush2color(item->data(StoredBackgroundRole)));
     writeOrDelete(group, "icon", item->data(IconNameRole));
