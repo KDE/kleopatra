6d7f1ce640d6a053669819ba76b37bb55321518a
Ingo Klöcker <dev@ingo-kloecker.de>
2026-06-16 18:05:49 +0200

    Unify name and email suggested for new certificates
    When creating a new OpenPGP certificate name and email were taken from
    the NAME and EMAIL settings. Only if those were not set and prefilling
    was not disabled explicitly then userFullName() and userEmailAddress()
    were used to retrieve the information from the system.
    
    Use the same logic for new S/MIME CSRs (which didn't consider explicitly
    configured NAME and EMAIL settings) and for new OpenPGP certificates
    created for smart cards (which also ignored NAME and EMAIL and, moreover,
    ignore the prefill settings).
    
    GnuPG-bug-id: 6094
    (cherry picked from commit 331d4d8c5eedaef123d4ecc399112d43aa8b460d)

---
+++ src/utils/settings-helpers.cpp
@@ -0,0 +1,51 @@
+/*
+    This file is part of Kleopatra, the KDE keymanager
+    SPDX-FileCopyrightText: 2026 g10 Code GmbH
+    SPDX-FileContributor: Ingo Klöcker <dev@ingo-kloecker.de>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "settings-helpers.h"
+
+#include "userinfo.h"
+
+#include <settings.h>
+
+using namespace Qt::StringLiterals;
+
+QString cnForNewCertificates()
+{
+    QString result;
+    const auto settings = Kleo::Settings{};
+    // prefer explicitly configured name over the values retrieved from the system
+    result = settings.name();
+    if (result.isEmpty() && settings.prefillCN()) {
+        result = Kleo::userFullName();
+    }
+    return result;
+}
+
+QString emailForNewCertificates()
+{
+    QString result;
+    const auto settings = Kleo::Settings{};
+    // prefer explicitly configured email over the values retrieved from the system
+    result = settings.email();
+    if (result.isEmpty() && settings.prefillEmail()) {
+        result = Kleo::userEmailAddress();
+    }
+    return result;
+}
+
+QString nameForNewCertificates()
+{
+    QString result;
+    const auto settings = Kleo::Settings{};
+    // prefer explicitly configured name over the values retrieved from the system
+    result = settings.name();
+    if (result.isEmpty() && settings.prefillName()) {
+        result = Kleo::userFullName();
+    }
+    return result;
+}
