d7fce90fe710f43594a48612f50bbf8880775828
Ingo Klöcker <dev@ingo-kloecker.de>
2026-06-17 17:05:10 +0200

    Restore Boolean *_prefill settings and add separate *_sources settings
    This restores the compatibility of the settings with older versions of
    Kleopatra and makes the code simpler.
    
    GnuPG-bug-id: 6094
    (cherry picked from commit 27b0f7913c153c28e2a27ed1fcfbdcc5c8a35eef)

--- src/utils/settings-helpers.cpp
+++ src/utils/settings-helpers.cpp
@@ -14,56 +14,14 @@
 
 using namespace Qt::StringLiterals;
 
-static QStringList prefillCN(const Kleo::Settings &settings)
-{
-    const QStringList prefillCNValue = settings.prefillCN();
-    if (prefillCNValue.size() == 1) {
-        if (prefillCNValue.front() == "true"_L1) {
-            return settings.defaultPrefillCNValue();
-        } else if (prefillCNValue.front() == "false"_L1) {
-            return {};
-        }
-    }
-    return prefillCNValue;
-}
-
-static QStringList prefillEmail(const Kleo::Settings &settings)
-{
-    const QStringList prefillEmailValue = settings.prefillEmail();
-    if (prefillEmailValue.size() == 1) {
-        if (prefillEmailValue.front() == "true"_L1) {
-            return settings.defaultPrefillEmailValue();
-        } else if (prefillEmailValue.front() == "false"_L1) {
-            return {};
-        }
-    }
-    return prefillEmailValue;
-}
-
-static QStringList prefillName(const Kleo::Settings &settings)
-{
-    const QStringList prefillNameValue = settings.prefillName();
-    if (prefillNameValue.size() == 1) {
-        if (prefillNameValue.front() == "true"_L1) {
-            return settings.defaultPrefillNameValue();
-        } else if (prefillNameValue.front() == "false"_L1) {
-            return {};
-        }
-    }
-    return prefillNameValue;
-}
-
 QString cnForNewCertificates()
 {
     QString result;
     const auto settings = Kleo::Settings{};
     // prefer explicitly configured name over the values retrieved from the system
     result = settings.name();
-    if (result.isEmpty()) {
-        const QStringList prefillCNValue = prefillCN(settings);
-        if (!prefillCNValue.empty()) {
-            result = Kleo::userFullName(prefillCNValue);
-        }
+    if (result.isEmpty() && settings.prefillCN()) {
+        result = Kleo::userFullName(settings.prefillSourcesCN());
     }
     return result;
 }
@@ -74,11 +32,8 @@ QString emailForNewCertificates()
     const auto settings = Kleo::Settings{};
     // prefer explicitly configured email over the values retrieved from the system
     result = settings.email();
-    if (result.isEmpty()) {
-        const QStringList prefillEmailValue = prefillEmail(settings);
-        if (!prefillEmailValue.empty()) {
-            result = Kleo::userEmailAddress(prefillEmailValue);
-        }
+    if (result.isEmpty() && settings.prefillEmail()) {
+        result = Kleo::userEmailAddress(settings.prefillSourcesEmail());
     }
     return result;
 }
@@ -89,11 +44,8 @@ QString nameForNewCertificates()
     const auto settings = Kleo::Settings{};
     // prefer explicitly configured name over the values retrieved from the system
     result = settings.name();
-    if (result.isEmpty()) {
-        const QStringList prefillNameValue = prefillName(settings);
-        if (!prefillNameValue.empty()) {
-            result = Kleo::userFullName(prefillNameValue);
-        }
+    if (result.isEmpty() && settings.prefillName()) {
+        result = Kleo::userFullName(settings.prefillSourcesName());
     }
     return result;
 }
