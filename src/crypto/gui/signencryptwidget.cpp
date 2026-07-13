e96a2ca380cb94583efb5d81e06de363fb2a3fbc
Ingo Klöcker <dev@ingo-kloecker.de>
2026-07-13 16:46:18 +0200

    Remove obsolete connect for changed encryption recipient
    The connect became obsolete when the Encrypt for others check box was
    re-added.
    If the mutual exclusive sign/encrypt mode is active and the current
    protocol is CMS and the Sign check box is checked then the Encrypt for
    others check box is unchecked. And if the Encrypt for others check box is
    unchecked then the recipient input fields are disabled so that it's
    impossible to get a keyChanged signal.
    
    GnuPG-bug-id: 8324

--- src/crypto/gui/signencryptwidget.cpp
+++ src/crypto/gui/signencryptwidget.cpp
@@ -475,15 +475,6 @@ CertificateLineEdit *SignEncryptWidget::Private::insertRecipientWidget(Certifica
         q->certificateSelectionRequested(recipient.edit);
     });
 
-    if (mIsExclusive) {
-        connect(recipient.edit, &CertificateLineEdit::keyChanged, q, [this]() {
-            if (mCurrentProto != GpgME::CMS) {
-                return;
-            }
-            mSigChk->setChecked(false);
-        });
-    }
-
     updateAllExpiryMessages();
 
     return recipient.edit;
